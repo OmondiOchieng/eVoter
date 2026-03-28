796074fb170273bf7aade371a79c6390fecba98c
Omondi Ochieng <150507369+OmondiOchieng@users.noreply.github.com>
2026-03-28 16:30:11 +0300

    Add configuration file for database and SMS settings

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "voting_system");
+
+// MobileSasa SMS
+define("SMS_API_KEY", "YOUR_API_KEY");
+define("SMS_SENDER_ID", "MOBILESASA");
+?>
